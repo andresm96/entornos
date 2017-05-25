5133596c0a90080f78301c7147f9d76ca4c7906a
neucoas <poma.nob@gmail.com>
2017-05-25 14:21:47 -0300

    updated side navbar

--- StoreWare/admin-cp.php
+++ StoreWare/admin-cp.php
@@ -46,10 +46,25 @@
                 <div class="col-md-3">
                     <p class="lead">Bienvenido Administrador</p>
                     <ul class="nav nav-pills nav-stacked">
-                        <li role="presentation" class="active"><a href="admin-cp.php">Listado</a></li>
-                        <li role="presentation" ><a href="admin-alta.php">Alta</a></li>
-                        <li role="presentation"><a href="admin-baja.php">Baja</a></li>
-                        <li role="presentation"><a href="admin-modif.php">Modificacion</a></li>
+                        <li role="presentation" class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Productos <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li role="presentation" class="active"><a href="admin-cp.php">Listado</a></li>
+                                <li role="presentation"><a href="admin-alta-prod.php">Alta</a></li>
+                                <li role="presentation"><a href="admin-baja-prod.php">Baja</a></li>
+                                <li role="presentation"><a href="admin-modif-prod.php">Modificacion</a></li>
+                            </ul>
+                        </li>
+
+                        <li role="presentation" class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Usuarios <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li role="presentation"><a href="admin-cp-user.php">Listado</a></li>
+                                <li role="presentation"><a href="admin-alta-user.php">Alta</a></li>
+                                <li role="presentation"><a href="admin-baja-user.php">Baja</a></li>
+                                <li role="presentation"><a href="admin-modif-user.php">Modificacion</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
 
