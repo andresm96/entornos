a46a6edd08f572aeb0102801117474254ff433dd
andresm96 <andres-malvestiti@hotmail.com>
2017-05-25 16:18:38 -0300

    registro de usuarios terminado

---
+++ StoreWare/php/login.php
@@ -0,0 +1,13 @@
+<?php
+  session_start();
+  $usuario=$_POST['usrname'];
+  $contraseña=$_POST['psw'];
+
+  function userExist(){
+
+    if(){
+      $_SESSION["usuario"]=;
+    }
+    return;
+  }
+ ?>
---
+++ StoreWare/php/singUp.php
@@ -0,0 +1,37 @@
+<?php
+
+  include("conexion.inc");
+  //Captura datos desde el Form anterior
+  $vNombre = $_POST['nombre'];
+  $vApellido = $_POST['apellido'];
+  $vDireccion = $_POST['direccion'];
+  $vTelefono = $_POST['telefono'];
+  $vEmail = $_POST['email'];
+  $vUsuario = $_POST['usrname'];
+  $vPassword = $_POST['psw'];
+
+  //Arma la instrucción SQL y luego la ejecuta
+  $vSql = "SELECT * FROM cliente WHERE usuario='$vUsuario'";
+  $vResultado = mysqli_query($con, $vSql) or die (mysqli_error($con));;
+  $vCantFilas = $vResultado->num_rows;
+
+  //$vCantUsuarios = mysqli_result($vResultado, 0);
+  if ($vCantFilas !=0){
+   echo ("El Usuario ya Existe<br>");
+   echo ("<A href='login.html'>Volver</A>");
+  }
+  else {
+    $vSql = "INSERT INTO cliente (nombre, apellido, direccion,
+                                  telefono, email, tipo_usu,
+                                  usuario, contrasenia)
+    values ('$vNombre','$vApellido', '$vDireccion', '$vTelefono',
+              '$vEmail', 0, '$vUsuario', '$vPassword')";
+    mysqli_query($con, $vSql) or die (mysqli_error($con));
+    echo("El Usuario fue Registrado<br><br>");
+    echo ("<A href='../index.html'>VOLVER AL MENU</A>");
+    // Liberar conjunto de resultados
+    mysqli_free_result($vResultado);
+  }
+  // Cerrar la conexion
+  mysqli_close($con);
+ ?>
