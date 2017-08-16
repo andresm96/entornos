a930a0f2c64bc140e69f365275a98c6d39baa4aa
andresm96 <andres-malvestiti@hotmail.com>
2017-08-16 15:49:21 -0300

    login page fixed and little index page implemention

--- StoreWare/php/login.php
+++ StoreWare/php/login.php
@@ -6,28 +6,25 @@
   $vUsuario=$_POST["username"];
   $vContraseña=$_POST["password"];
 
-  $vSql = "SELECT usuario FROM cliente WHERE usuario='$vUsuario'";
+  $vSql = "SELECT * FROM cliente WHERE usuario='$vUsuario'";
   $vResultado = mysqli_query($con, $vSql) or die (mysqli_error($con));
   $vCantFilas = mysqli_num_rows($vResultado);
+  $vExtraido = mysqli_fetch_array($vResultado);
 
   if ($vCantFilas ==0){
     echo ("El Usuario no Existe<br><br>");
     echo ("<A href='../login.html'>Volver</A>");
   }
   else{
-    $vFila = mysqli_fetch_assoc($vResultado);
-    $vExtraido = mysqli_fetch_array($vFila,0);
-    $_SESSION["usuario"] = $vExtraido["usuario"];
-    $_SESSION["admin"] = isAdmin($vExtraido);
+    $_SESSION["usuario"] = $vExtraido['usuario'];
+    if($vExtraido['tipo_usu']==1){
+      $_SESSION["admin"]=true;
+    }
+    else {
+      $_SESSION["admin"] = false;
+    }
     echo ("Login exitoso<br><br>");
-    echo ("<A href='../index.html'>Volver</A>");
+    echo ("<A href='../index.php'>Volver</A>");
   }
 
-  function isAdmin($vExtraido){
-    $vAdmin=false;
-    if($vExtraido[7]==1){
-      $vAdmin=true;
-    }
-    return $vAdmin;
-  }
  ?>
