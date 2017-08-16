245499eff81941e6732e3726040712a39a21f6d2
julianpoma <poma.nob@gmail.com>
2017-08-16 16:41:59 -0300

    full implementation of product modify

--- StoreWare/php/commit-modif.php
+++ StoreWare/php/commit-modif.php
@@ -1,38 +1,26 @@
 <?php
+    session_start();
+    $conn = mysqli_connect("localhost", "root") or die ("Problemas de conexion a la base de datos");
+    mysqli_select_db($conn, "storeware");
 
-    if (isset($_POST["submit"])) {
+    $id = $_SESSION["idprod"];
+    $nombre = $_POST['nombre'];
+    $precio = $_POST['precio'];
+    $stock = $_POST['stock'];
 
-        $conn = mysqli_connect("localhost", "root") or die ("Problemas de conexion a la base de datos");
-        mysqli_select_db($conn, "storeware");
+    $sqlUpdate = "UPDATE producto SET nombre='$nombre', precio='$precio', stock='$stock' WHERE id_producto=$id";
 
-        $bajaId = $_POST['idbaja'];
+    $resultado=mysqli_query($conn,$sqlUpdate) or die (mysqli_error($conn));
 
-        //Hacemos la consulta para verificar si el ID existe
-
-        $consulta = "SELECT * from producto WHERE id_producto=$bajaId";
-
-        $resultado=mysqli_query($conn, $consulta) or die (mysqli_error($conn));
-
-        if(mysqli_num_rows($resultado) != 0) {
-
-            $consulta = "DELETE FROM producto WHERE id_producto=$bajaId";
-            $resultado=mysqli_query($conn, $consulta);
-
-            if($resultado) {
-                $resultado = '<div class="alert alert-success">El producto se ha eliminado exitosamente!</div>';
-            }
-            else {
-                $resultado = '<div class="alert alert-danger">Ha habido un error al eliminar el producto</div>';
-            }
-        }
-        else {
-            
-            $resultado = '<div class="alert alert-danger">Ha habido un error al eliminar el producto</div>';
-        }
-
-        // mysqli_free_result($resultado);
-        mysqli_close($conn);
+    if($resultado)
+    {
+        $success = '<div class="alert alert-success">El producto se ha modificado exitosamente!</div>';
     }
+    else {
+        $success = '<div class="alert alert-danger">Ha ocurrido un error inexplicable!</div>';
+    }
+
+    mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
@@ -46,9 +34,9 @@
 
         <title>Administrator CP</title>
 
-        <link href="css/styles-css/cp-styles.css" rel="stylesheet">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/shop-homepage.css" rel="stylesheet">
+        <link href="../css/styles-css/cp-styles.css" rel="stylesheet">
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/shop-homepage.css" rel="stylesheet">
 
     </head>
 
@@ -86,42 +74,34 @@
                         <li role="presentation" class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Productos <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li role="presentation"><a href="admin-cp.php">Listado</a></li>
-                                <li role="presentation"><a href="admin-alta-prod.php">Alta</a></li>
-                                <li role="presentation" class="active"><a href="admin-baja-prod.php">Baja</a></li>
-                                <li role="presentation"><a href="admin-modif-prod.php">Modificacion</a></li>
+                                <li role="presentation"><a href="../admin-cp.php">Listado</a></li>
+                                <li role="presentation"><a href="../admin-alta-prod.php">Alta</a></li>
+                                <li role="presentation"><a href="../admin-baja-prod.php">Baja</a></li>
+                                <li role="presentation" class="active"><a href="../admin-modif-prod.php">Modificacion</a></li>
                             </ul>
                         </li>
 
                         <li role="presentation" class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Usuarios <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li role="presentation"><a href="admin-cp-user.php">Listado</a></li>
-                                <li role="presentation"><a href="admin-alta-user.php">Alta</a></li>
-                                <li role="presentation"><a href="admin-baja-user.php">Baja</a></li>
-                                <li role="presentation"><a href="admin-modif-user.php">Modificacion</a></li>
+                                <li role="presentation"><a href="../admin-cp-user.php">Listado</a></li>
+                                <li role="presentation"><a href="../admin-alta-user.php">Alta</a></li>
+                                <li role="presentation"><a href="../admin-baja-user.php">Baja</a></li>
+                                <li role="presentation"><a href="../admin-modif-user.php">Modificacion</a></li>
                             </ul>
                         </li>
                     </ul>
                 </div>
-
+                
                 <div class="col-md-7 col-md-offset-1">
-                    <h1>Baja de un producto</h1> <hr>
+                    <h1>Modificar un producto</h1>
+                    <hr>
 
-                    <form class="form-group" action="admin-baja-prod.php" method="post">
-                        <div class="form-group">    
-                            <input class="form-control" type="number" min="0" name="idbaja" placeholder="Ingrese el ID del producto a eliminar" required>
-                        </div>
-                        <button type="reset" value="Reset" class="btn btn-default" >Limpiar</button>
-                        <input type="submit" class="btn btn-danger pull-right" name="submit" value="Eliminar">
-                        <br><br>
-                        <?php
-                            if (isset($_POST["submit"])) {
-                                echo $resultado;
-                            }
-                        ?>
-
-                    </form>
+                    <?php
+                        echo $success;
+                    ?>
+                    
+                    <a href="../admin-modif-prod.php"><button type="button" class="btn btn-primary">Volver al menu de modificacion</button></a>
                 </div>
             </div>
         </div>
@@ -140,8 +120,8 @@
         <!-- end footer -->
 
         <!-- jQuery -->
-        <script src="js/jquery.js"></script>
+        <script src="../js/jquery.js"></script>
         <!-- Bootstrap Core JavaScript -->
-        <script src="js/bootstrap.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
     </body>
 </html>
